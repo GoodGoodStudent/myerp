23287e4864828742fc28cc9ace63da6c2cbc7355
GoodGoodStudent <759812278@qq.com>
2019-12-18 14:37:10 +0800

    first commit

---
+++ src/main/java/com/puhuanyu/erp/myerp/MyerpApplication.java
@@ -0,0 +1,15 @@
+package com.puhuanyu.erp.myerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyerpApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MyerpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puhuanyu/erp/myerp/MyerpApplicationTests.java
@@ -0,0 +1,15 @@
+package com.puhuanyu.erp.myerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyerpApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
