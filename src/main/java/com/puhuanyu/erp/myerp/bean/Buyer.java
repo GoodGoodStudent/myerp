b668635f2ac9b70af14b302958b46af5ffd7313c
GoodGoodStudent <759812278@qq.com>
2019-12-20 15:23:47 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: GoodGoodStudent <759812278@qq.com>

--- src/main/java/com/puhuanyu/erp/myerp/bean/Buyer.java
+++ src/main/java/com/puhuanyu/erp/myerp/bean/Buyer.java
@@ -11,13 +11,30 @@ public class Buyer
     private Dep dep;
     private Emp emp1;//审核员
     private Emp emp2;//采购员
+    private Goodsinfo goodsinfo;
+    private int number;
+    private int state;//0表示未完成，1表示已完成
 
 
     public Buyer()
     {
     }
 
-
+    public Buyer(int id, String numberid, String builddate, String auditdate, String begaindate, String overdate, Dep dep, Emp emp1, Emp emp2, Goodsinfo goodsinfo, int number, int state)
+    {
+        this.id = id;
+        this.numberid = numberid;
+        this.builddate = builddate;
+        this.auditdate = auditdate;
+        this.begaindate = begaindate;
+        this.overdate = overdate;
+        this.dep = dep;
+        this.emp1 = emp1;
+        this.emp2 = emp2;
+        this.goodsinfo = goodsinfo;
+        this.number = number;
+        this.state = state;
+    }
 
     public int getId()
     {
@@ -87,5 +104,55 @@ public class Buyer
     public void setDep(Dep dep)
     {
         this.dep = dep;
+    }
+
+    public Emp getEmp1()
+    {
+        return emp1;
+    }
+
+    public void setEmp1(Emp emp1)
+    {
+        this.emp1 = emp1;
+    }
+
+    public Emp getEmp2()
+    {
+        return emp2;
+    }
+
+    public void setEmp2(Emp emp2)
+    {
+        this.emp2 = emp2;
+    }
+
+    public Goodsinfo getGoodsinfo()
+    {
+        return goodsinfo;
+    }
+
+    public void setGoodsinfo(Goodsinfo goodsinfo)
+    {
+        this.goodsinfo = goodsinfo;
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(int number)
+    {
+        this.number = number;
+    }
+
+    public int getState()
+    {
+        return state;
+    }
+
+    public void setState(int state)
+    {
+        this.state = state;
     }
 }
--- src/main/java/com/puhuanyu/erp/myerp/bean/Goodsinfo.java
+++ src/main/java/com/puhuanyu/erp/myerp/bean/Goodsinfo.java
@@ -9,4 +9,89 @@ public class Goodsinfo
     private Goodstype goodstype;
     private int number;
     private Warehouse warehouse;
+
+    public Goodsinfo()
+    {
+    }
+
+    public Goodsinfo(int id, String name, Client client, String unit, Goodstype goodstype, int number, Warehouse warehouse)
+    {
+        this.id = id;
+        this.name = name;
+        this.client = client;
+        this.unit = unit;
+        this.goodstype = goodstype;
+        this.number = number;
+        this.warehouse = warehouse;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Client getClient()
+    {
+        return client;
+    }
+
+    public void setClient(Client client)
+    {
+        this.client = client;
+    }
+
+    public String getUnit()
+    {
+        return unit;
+    }
+
+    public void setUnit(String unit)
+    {
+        this.unit = unit;
+    }
+
+    public Goodstype getGoodstype()
+    {
+        return goodstype;
+    }
+
+    public void setGoodstype(Goodstype goodstype)
+    {
+        this.goodstype = goodstype;
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(int number)
+    {
+        this.number = number;
+    }
+
+    public Warehouse getWarehouse()
+    {
+        return warehouse;
+    }
+
+    public void setWarehouse(Warehouse warehouse)
+    {
+        this.warehouse = warehouse;
+    }
 }
