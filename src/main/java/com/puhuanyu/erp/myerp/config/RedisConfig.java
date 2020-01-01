101195f36128dfeeb1525556b7b24951b65d298a
Administrator <759812278@qq.com>
2020-01-01 17:19:30 +0800

    添加了redisconfig，修改了redisutil,无论是集合或者对象都转换成json字符串存入redis中，涉及增删改，直接删除该类的所有缓存

---
+++ src/main/java/com/puhuanyu/erp/myerp/config/RedisConfig.java
@@ -0,0 +1,40 @@
+package com.puhuanyu.erp.myerp.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ *  @Description 这是一个redis缓存配置类，主要定义数据存入redis缓存的序列化方式
+ *  @ClassName RedisConfig
+ *  @Author 忠哥
+ *  @data 2019-12-31 13:52
+ */
+@Configuration
+public class RedisConfig {
+    /**
+     * @Description key和hash的key,value都采用string方式存入缓存
+     * @Param [factory] redis连接工厂
+     * @return org.springframework.data.redis.core.RedisTemplate<java.lang.String,java.lang.Object> 返回一个redisTemplate
+     * @Author 忠哥
+     * @Date 2019-12-31 13:55
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();//创建一个redisTemplate
+        template.setConnectionFactory(factory);//连接工厂
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();//字符串序列化
+        template.setKeySerializer(stringRedisSerializer);//key序列化成String
+        template.setHashKeySerializer(stringRedisSerializer);//hash的key序列化成String
+        template.setValueSerializer(stringRedisSerializer);//value序列化成String
+        template.setHashValueSerializer(stringRedisSerializer);//hash的value序列化成String
+        template.afterPropertiesSet();//初始化template
+        return template;
+    }
+}
--- src/main/java/com/puhuanyu/erp/myerp/controller/HelleoController.java
+++ src/main/java/com/puhuanyu/erp/myerp/controller/HelleoController.java
@@ -1,15 +1,25 @@
 package com.puhuanyu.erp.myerp.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.puhuanyu.erp.myerp.bean.Roottype;
 import com.puhuanyu.erp.myerp.mapper.RootMapper;
 import com.puhuanyu.erp.myerp.mapper.RootTypeMapper;
 import com.puhuanyu.erp.myerp.bean.Root;
 import com.puhuanyu.erp.myerp.service.RootTypeService;
+import com.puhuanyu.erp.myerp.util.RedisTemplateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @RestController
@@ -19,10 +29,62 @@ public class HelleoController
     private RootMapper rootMapper;
     @Autowired
     private RootTypeService rootTypeService;
+    @Autowired
+    private Roottype roottype;
+    @Autowired
+    private RedisTemplateUtil redisTemplateUtil;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @RequestMapping("/hello")
-    public String hello()
-    {
-        //return rootTypeService.findRootTypeById(1).toString();
-        return rootTypeService.findRootTypeAll().toString();
+    public void hello(HttpServletResponse response, HttpServletRequest request) {
+        response.setCharacterEncoding("utf-8");
+        String key = "";
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            key = redisTemplateUtil.findObject("Roottype", 1);
+            if(key == null){
+                roottype = rootTypeService.findRootTypeById(1);
+                redisTemplateUtil.setObject("Roottype", roottype, 1);
+                out.println(JSON.toJSONString(roottype));
+            }else {
+                out.println(key);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            out.flush();
+            out.close();
+        }
+    }
+
+    @RequestMapping("/world")
+    public void world(HttpServletResponse response, HttpServletRequest request){
+        response.setCharacterEncoding("utf-8");
+        String key = "";
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            key = redisTemplateUtil.findObject("Roottype", null);
+            if(key == null){
+                List<Roottype> list = rootTypeService.findRootTypeAll();
+                redisTemplateUtil.setObject("Roottype", list, null);
+                out.println(JSONArray.toJSONString(list));
+            }else {
+                out.println(key);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            out.flush();
+            out.close();
+        }
+    }
+
+    @RequestMapping("/wc")
+    public void wc(HttpServletResponse response, HttpServletRequest request){
+        rootTypeService.updateRootType(new Roottype(6, "仓库管理"));
+        redisTemplateUtil.delKey("Roottype");
     }
 }
